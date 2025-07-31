c0eb88e25c490b0e22799891c94fe0dd06085174
akshprooo <akshprooo@gmail.com>
2025-07-31 13:10:24 +0530

    Update Project.js

--- src/data/Project.js
+++ src/data/Project.js
@@ -3,8 +3,10 @@ const ProjectsData = [
     {name: 'Docs App', description: 'Very Simple Docs App with React and Framer Motion.', github:'https://github.com/akshprooo/DocsApp', live:'https://docs-app-orpin-tau.vercel.app/', techStack: ['React', 'JavaScript', 'Vite', 'Tailwindcss'], markup: './images/docsapp.png'},
     {name: 'SoSE Landing Page', description: 'Institution Landing Page.', github:'https://github.com/akshprooo/sose', live:'https://sose-cyan.vercel.app/', techStack: ['React', 'JavaScript', 'Vite', 'Tailwindcss'], markup: './images/sose.png'},
     {name: 'Demon Slayer Characters', description: 'Utilisation Of the DemonSlayer API', github:'https://github.com/akshprooo/demon_slayer-characters/', live:'https://demonslayer-kappa.vercel.app/', techStack: ['React', 'TypeScript', 'NextJS', 'Tailwindcss'], markup: './images/demonslayer.png'},
+    {name: 'SoSE Landing Page', description: 'Institution Landing Page.', github:'https://github.com/akshprooo/oh.studio', techStack: ['HTML','CSS','JavaScript','GSAP', 'Vite']},
     {name: 'BetterFileHandling', description: 'A Python Package for File Management.', github:'https://github.com/akshprooo/BetterFileHandling', live:'https://pypi.org/project/BetterFileHandling/', techStack: ['Python'], markup: './images/betterfilehandling.png'},
     {name: 'LocalUserDB', description: 'A Python Package for User Management.', live:'https://pypi.org/project/LocalUserDB/', techStack: ['Python'], markup: './images/localuserdb.jpeg'},
+    {name: 'PyOS', description: 'An OS Like Environment being developed purely in python.', techStack: ['Python'], github:'https://github.com/akshprooo/PyOS'},
 ]
 
 export default ProjectsData
