d63a3630fe527cf85d36ae2ed285f8d73f8b2d05
akshprooo <akshprooo@gmail.com>
2025-08-31 22:46:19 +0530

    Update Project.js

--- src/data/Project.js
+++ src/data/Project.js
@@ -1,11 +1,12 @@
 const ProjectsData = [
-    {name: 'Atlas Software Website', description: 'This Was the Creation of Atlas Software Website', live:'https://atlas-software.org', techStack: ['HTML', 'CSS', 'JS'], markup: './images/atlassoft.jpeg'},
-    {name: 'Docs App', description: 'A Docs App with a sleek user interface and smooth animations.', github:'https://github.com/akshprooo/DocsApp', live:'https://docs-app-orpin-tau.vercel.app/', techStack: ['React', 'JavaScript', 'Vite', 'Tailwindcss', 'Framer-Motion'], markup: './images/docsapp.png'},
-    {name: 'SoSE Landing Page', description: 'Landing Page for Institution with a modern design and responsive layout.', github:'https://github.com/akshprooo/sose', live:'https://sose-cyan.vercel.app/', techStack: ['React', 'JavaScript', 'Vite', 'Tailwindcss', 'GSAP'], markup: './images/sose.png'},
-    {name: 'Demon Slayer Characters', description: 'A Webapp showcasing characters from the Demon Slayer series.', github:'https://github.com/akshprooo/demon_slayer-characters/', live:'https://demonslayer-kappa.vercel.app/', techStack: ['React', 'TypeScript', 'NextJS', 'Tailwindcss'], markup: './images/demonslayer.png'},
+    {name: 'Atlas Software Website', description: 'This Was the Creation of Atlas Software Website', live:'https://atlas-software.org', techStack: ['HTML', 'CSS', 'JS']},
+    {name: 'ZoopCloudera Website', description: 'The Official Website of ZoopCloudera IT Solutions.', live:'https://zoopcloudera.in', techStack: ['React', 'TailwindCSS', 'GSAP', 'react-router-dom', 'vite']},
+    {name: 'Docs App', description: 'A Docs App with a sleek user interface and smooth animations.', github:'https://github.com/akshprooo/DocsApp', live:'https://docs-app-orpin-tau.vercel.app/', techStack: ['React', 'JavaScript', 'Vite', 'Tailwindcss', 'Framer-Motion']},
+    {name: 'SoSE Landing Page', description: 'Landing Page for Institution with a modern design and responsive layout.', github:'https://github.com/akshprooo/sose', live:'https://sose-cyan.vercel.app/', techStack: ['React', 'JavaScript', 'Vite', 'Tailwindcss', 'GSAP']},
+    {name: 'Demon Slayer Characters', description: 'A Webapp showcasing characters from the Demon Slayer series.', github:'https://github.com/akshprooo/demon_slayer-characters/', live:'https://demonslayer-kappa.vercel.app/', techStack: ['React', 'TypeScript', 'NextJS', 'Tailwindcss']},
     {name: 'Oh.Studio', description: 'Recreation of a landing page (done under a challenge of 1 hour :) )', github:'https://github.com/akshprooo/oh.studio', techStack: ['HTML','CSS','JavaScript','GSAP', 'Vite']},
-    {name: 'BetterFileHandling', description: 'A Python Package for File Management.', github:'https://github.com/akshprooo/BetterFileHandling', live:'https://pypi.org/project/BetterFileHandling/', techStack: ['Python'], markup: './images/betterfilehandling.png'},
-    {name: 'LocalUserDB', description: 'A Python Package for User Management.', live:'https://pypi.org/project/LocalUserDB/', techStack: ['Python'], markup: './images/localuserdb.jpeg'},
+    {name: 'BetterFileHandling', description: 'A Python Package for File Management.', github:'https://github.com/akshprooo/BetterFileHandling', live:'https://pypi.org/project/BetterFileHandling/', techStack: ['Python']},
+    {name: 'LocalUserDB', description: 'A Python Package for User Management.', live:'https://pypi.org/project/LocalUserDB/', techStack: ['Python']},
     {name: 'PyOS', description: 'An OS Like Environment being developed purely in python.', techStack: ['Python'], github:'https://github.com/akshprooo/PyOS'},
 ]
 
