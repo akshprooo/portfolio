7a3bb9858d2a81be65767f6ee76d05d483630b46
akshprooo <akshprooo@gmail.com>
2025-09-02 15:45:02 +0530

    new projecty

--- src/data/Project.js
+++ src/data/Project.js
@@ -1,6 +1,7 @@
 const ProjectsData = [
     {name: 'Atlas Software Website', description: 'This Was the Creation of Atlas Software Website', live:'https://atlas-software.org', techStack: ['HTML', 'CSS', 'JS']},
     {name: 'ZoopCloudera Website', description: 'The Official Website of ZoopCloudera IT Solutions.', live:'https://zoopcloudera.in', techStack: ['React', 'TailwindCSS', 'GSAP', 'react-router-dom', 'vite']},
+    {name: 'TechMyAir Website', description: 'A full stack e-commerce webapp with beautiful design and modern functionality.', live:'https://techmyair.vercel.app', techStack: ['React', 'TailwindCSS', 'Supabase', 'react-router-dom', 'vite']},
     {name: 'Docs App', description: 'A Docs App with a sleek user interface and smooth animations.', github:'https://github.com/akshprooo/DocsApp', live:'https://docs-app-orpin-tau.vercel.app/', techStack: ['React', 'JavaScript', 'Vite', 'Tailwindcss', 'Framer-Motion']},
     {name: 'SoSE Landing Page', description: 'Landing Page for Institution with a modern design and responsive layout.', github:'https://github.com/akshprooo/sose', live:'https://sose-cyan.vercel.app/', techStack: ['React', 'JavaScript', 'Vite', 'Tailwindcss', 'GSAP']},
     {name: 'Demon Slayer Characters', description: 'A Webapp showcasing characters from the Demon Slayer series.', github:'https://github.com/akshprooo/demon_slayer-characters/', live:'https://demonslayer-kappa.vercel.app/', techStack: ['React', 'TypeScript', 'NextJS', 'Tailwindcss']},
